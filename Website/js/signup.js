2ed16969a6caea0344c78f58c417aee26bd70a32
Jordan Reyna <jreynatheman@gmail.com>
2018-04-10 16:56:04 -0400

    Add files via upload

--- Website/js/signup.js
+++ Website/js/signup.js
@@ -1,5 +1,4 @@
 // Get the modal
-// Get the modal
 var modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
@@ -8,3 +7,22 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+var frmvalidator = new Validator("id01");
+	frmvalidator.EnableOnPageErrorDisplay();
+	frmvalidator.EnableMsgsTogether();
+
+	frmvalidator.addValidation("fname","req","Please enter your First Name");
+	frmvalidator.addValidation("fname","maxlen=30","Max length for FirstName is 20");
+	frmvalidator.addValidation("lname","req");
+	frmvalidator.addValidation("lname","maxlen=30");
+	frmvalidator.addValidation("email","req");
+	frmvalidator.addValidation("email","maxlen=255");
+	frmvalidator.addValidation("Email","email");
+	frmvalidator.addValidation("usr","req");
+	frmvalidator.addValidation("usr","maxlen=30");
+	frmvalidator.addValidation("psw","req");
+	frmvalidator.addValidation("psw","maxlen=50");
+	frmvalidator.addValidation("psw-repeat","req");
+	frmvalidator.addValidation("psw-repeat","maxlen=30");
+
