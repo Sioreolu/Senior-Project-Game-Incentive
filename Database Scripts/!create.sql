ffb66a848d23fc83969862b03821f80316b62f4d
Jordan Reyna <jreynatheman@gmail.com>
2018-04-10 15:37:27 -0400

    Add files via upload

--- Database Scripts/!create.sql
+++ Database Scripts/!create.sql
@@ -1,7 +1,9 @@
-drop schema if exists Game;
-create schema Game;
+use reynaj;# MySQL returned an empty result set (i.e. zero rows).
 
-use Game;
+Drop table if exists Users;# MySQL returned an empty result set (i.e. zero rows).
+
+Drop table if exists Scores;# MySQL returned an empty result set (i.e. zero rows).
+
 
 create table Users (
 userID int(11) auto_increment,
@@ -12,17 +14,17 @@ firstName varchar(30),
 lastName varchar(30),
 PRIMARY KEY (userID),
 UNIQUE KEY (userName)
-);
+);# MySQL returned an empty result set (i.e. zero rows).
+
 
 create table Scores (
 scoreID int(11) auto_increment,
 userID int(11),
 score int(10),
 scoreDate datetime,
-PRIMARY KEY (userID),
-PRIMARY KEY (userID),
+PRIMARY KEY (scoreID),
 FOREIGN KEY fk_user(userID)
 REFERENCES Users(userID)
 ON UPDATE CASCADE
 ON DELETE RESTRICT
-);
+);# MySQL returned an empty result set (i.e. zero rows).
