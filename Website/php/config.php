fa65a949016b2ebe596d2e7f1afa7f70e2167775
Jordan Reyna <jreynatheman@gmail.com>
2018-04-18 12:52:54 -0400

    Add files via upload

--- Website/php/config.php
+++ Website/php/config.php
@@ -1,20 +1,16 @@
 <?PHP
-require_once("/functions.php");
+echo "Test";
 
-$gamesite = new gameSite();
+var $servername = "https://cis.stvincent.edu/pma";
+var $username = "reynaj";
+var $password = "Db=pwd768";
+var $dbname = "reynaj";
 
-//Site Name
-$gamesite->SetWebsiteName('cis.stvincent.edu/reynaj');
+$con=mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
 
-//Email Address Notification
-$gamesite->SetAdminEmail('user11@user11.com');
-
-//Database login
-$gamesite->InitDB('localhost','reynaj','redB=pwd946','reynaj','game');
-
-/* Not sure?
-//For better security. Get a random string from this link: http://tinyurl.com/randstr
-// and put it here
-$gamesite->SetRandomKey('qSRcVS6DrTzrPvr');
-*/
 ?>
